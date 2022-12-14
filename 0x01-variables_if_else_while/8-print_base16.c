13373c82ed6d5c2fbfc54e2c37e40493e5efbc66
Meckson Mthembu <Macksontibade@gmail.com>
2022-12-14 05:33:55 -0800

    8. Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	char ch;
 
-	for (ch = '0'; ch < '9'; ch++)
+	for (ch = '0'; ch <= '9'; ch++)
 	{
 		putchar(ch);
 	}
-	for (ch = 'a'; ch < 'f'; ch++)
+	for (ch = 'a'; ch <= 'e'; ch++)
 	{
 		putchar(ch);
 	}
