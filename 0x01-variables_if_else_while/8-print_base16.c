0647db40f75800fdb2d26a361b27c708d54bce42
Meckson Mthembu <Macksontibade@gmail.com>
2022-12-14 05:22:37 -0800

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - program that print all the numbers of base 16
+ *
+ * Return:0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '0'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(ch);
+	return (0);
+}
