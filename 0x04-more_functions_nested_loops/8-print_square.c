a0ccd295901cd609e3b6679473a71b3ef0a3867a
Meckson Mthembu <Macksontibade@gmail.com>
2022-12-16 06:56:06 -0800

    8. You are so much sunshine in every square inch

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - print hashes squares
+ * @size: size of the square
+ *
+ * Return: no return
+ */
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar(35);
+		}
+		if (i != size - 1)
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
