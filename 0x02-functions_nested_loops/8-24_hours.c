7bb87400995be38075cf70d0cdb1e24a3d1eb660
Meckson Mthembu <Macksontibade@gmail.com>
2022-12-24 05:08:02 -0800

    8. I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,45 +1,47 @@
 #include "main.h"
 
 /**
- * jack_bauer - Prints every minute of the day of Jack Bauer
- * Return: void
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * Return: 0
  */
-void jack_bauer(void);
+void jack_bauer(void)
 {
-int h1;
-int h2;
-int m1;
-int m2;
-int a = 9;
+	int h1;
+	int h2;
+	int m1;
+	int m2;
+	int a = 9;
 
-h2 = 0;
-while (h2 <= 2)
-{
-	if (h2 == 2)
+	h2 = 0;
+	while (h2 <= 2)
 	{
-		a = 3;
-	}
-	h1 = 0;
-	while (h1 <= a)
-	{
-		m2 = 0;
-		while (m2 <= 5)
+		if (h2 == 2)
 		{
-			m1 = 0;
-			while (m1 <= 9)
-			{
-				_putchar('0' + h2);
-				_putchar('0' + h1);
-				_putchar(':');
-				_putchar('0' + m2);
-				_putchar('0' + m1);
-				_putchar('\n');
-				m1++;
-			}
-			m2++;
+			a = 3;
 		}
-		h1++;
+
+		h1 = 0;
+		while (h1 <= a)
+		{
+			m2 = 0;
+			while (m2 <= 5)
+			{
+				m1 = 0;
+				while (m1 <= 9)
+				{
+					_putchar(h2 + '0');
+					_putchar(h1 + '0');
+					_putchar(':');
+					_putchar(m2 + '0');
+					_putchar(m1 + '0');
+					_putchar('\n');
+					m1++;
+				}
+				m2++;
+			}
+			h1++;
+		}
+		h2++;
 	}
-	h2++;
-}
 }
