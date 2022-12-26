789cf3ab7e8bf9f69f9dff45d4ab8a98538456d7
Meckson Mthembu <Macksontibade@gmail.com>
2022-12-26 10:52:50 -0800

    12. Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <main.h>
+
+/**
+ * main - prints combinations of two two-digits of numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+	int num3;
+	int num4;
+
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		for (num2 = 0; num2 < 10; num2++)
+		{
+			for (num3 = 0; num3 < 10; num3++)
+			{
+				for (num4 = 0; num4 < 10; num4++)
+				{
+					if (num1 + num2 < num3 + num4)
+					{
+						putchar(num1 + '0');
+						putchar(num2 + '0');
+						putchar(' ');
+						putchar(num3 + '0');
+						putchar(num4 + '0');
+						if (num1 + num2 + num3 + num4 < 36)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	return (0);
+}
