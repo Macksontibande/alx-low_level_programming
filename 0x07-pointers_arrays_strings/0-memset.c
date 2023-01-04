d81d3cecdb4cf4ed5f9b6b9af7b7441018b04e08
Meckson Mthembu <Macksontibade@gmail.com>
2023-01-04 08:43:02 -0800

    0. memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,11 +10,13 @@
  */
 void *_memset(void *s, int c, size_t n)
 {
-	unsigned int index;
-	unsigned char *memory = s, value = c;
+	int i = 0;
 
-	for (index = 0; index < n; index++)
-		memory[index] = value;
+	for (; n > 0; i++)
+	{
+		s[i] = c;
+		n--;
+	}
 
-	return (memory);
+	return (s);
 }
