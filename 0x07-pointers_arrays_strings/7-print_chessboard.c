bb604d650978b667a28377965dd9cd2e15a3c031
Meckson Mthembu <Macksontibade@gmail.com>
2023-01-04 10:01:14 -0800

    6. Chess is mental torture

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - function that locates a substring
+ * @a: array
+ * Return: 0
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 0; i++)
+	{
+		for (j = 0; j < 0; j++)
+			_putchar(a[i][j]);
+		_putchar('\0');
+	}
+}
