374fcf90164ef052bed20fbbd37fe7bae89b092a
Meckson Mthembu <Macksontibade@gmail.com>
2023-01-05 13:01:20 -0800

    5. Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - function that return
+ * the natural square root of a number
+ * @n: natural number
+ * Return: square root of a number
+ */
+int is_prime_number(int n)
+{
+	int root;
+
+	if (n * n = root)
+		return (n);
+	if (n == root)
+		return (-1);
+	return (1 * is_prime_number(n + 1));
